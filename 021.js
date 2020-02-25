f06bbc984c3a33fe2360e0844b484ea5a2e738b9
Ninn Langel <info@ninnlangel.com>
2020-02-25 14:35:54 +0100

    Refactored 21

--- 021.js
+++ 021.js
@@ -1,32 +1,65 @@
-const MAXIMUM = 10000;
+/* 
+Project Euler Problem 21
+
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly 
+into n). If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a 
+and b are called amicable numbers. For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 
+11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 
+and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
+*/
+
+// Timing the execution :
+let startTime = Date.now()
+
+// Declaring functions
+function isDivisor(num, div) {
+    return num % div == 0;
+}
 
 function sumOfProperDivisors(number) {
+    let length = Math.floor(number / 2);
     let sum = 0;
-    for (let i = 1; i <= Math.floor(number / 2); i++) {
-        ((number % i) == 0) ? (sum += i) : {};
+    for (let i = 1; i <= length; i++) {
+        if (isDivisor(number, i)) {
+            sum += i
+        }
     }
     return sum;
 }
 
-function amicablePair(number) {
-    let sum = sumOfProperDivisors(number)
-    if (sum <= MAXIMUM && sumOfProperDivisors(sum) == number && sum != number) {
-        return sum;
+function getAmicablePair(number) {
+    let amicablePair = sumOfProperDivisors(number);
+    if (sumOfProperDivisors(amicablePair) == number && amicablePair != number) {
+        return amicablePair;
     } else {
-        return false;
+        return null;
     }
 }
 
-let result = [];
-
-for (let i = 0; i <= MAXIMUM; i++) {
-    let number = amicablePair(i);
-    if (number != false) {
-        result.push(number, i)
+function findAmicablePairsWithinRange(rangeMin, rangeMax) {
+    let amicablePairs = new Set(); // Using a set avoids having to manage duplicates
+    for (let i = rangeMin; i <= rangeMax; i++) {
+        let pair = getAmicablePair(i);
+        if (pair != null) {
+            if (pair <= rangeMax) {
+                amicablePairs.add(pair);
+                amicablePairs.add(i);
+            }
+        }
     }
+    return amicablePairs;
 }
 
-result = Array.from(new Set(result));
+// Running with exercise specifics :
+let amicablePairs = findAmicablePairsWithinRange(1, 100000);
+
+// Summing pairs and displaying result.
+let result = Array.from(amicablePairs); // Must convert set to array for reduction.
 result = result.reduce((sum, n) => sum + n);
 
-console.log(result);
+console.log(result)
+
+// Display time the program ran.
+console.log("duration : ", Date.now() - startTime, "ms");
