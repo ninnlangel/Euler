472371add8d86a7f19ef704fdbd2fa8db5b5bf96
Ninn Langel <info@ninnlangel.com>
2020-03-14 01:17:46 +0100

    added problem def of 24

--- 024.js
+++ 024.js
@@ -4,8 +4,13 @@
     
     Generated on : Fri Mar 13 2020
 
-    Insert problem definition here.
+    A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation 
+    of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, 
+    we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
 
+    012   021   102   120   201   210
+
+    What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 */
 
 // Timing the execution :
@@ -26,27 +31,26 @@ function factorial(number) {
 }
 
 function permutationAtPos(position, sample) {
-    if (sample.length == 1) {
-        result.push(sample[0])
+    if (position > factorial(sample.length)) {
+        result = "Position is beyond the number of possibilities"
     } else {
-        num = Math.floor(position / factorial(sample.length - 1));
-        result.push(sample[num]);
-        sample.splice(num, 1);
-        permutationAtPos(position % factorial(sample.length), sample);
-
+        if (sample.length == 1) {
+            result.push(sample[0])
+        } else {
+            num = Math.floor(position / factorial(sample.length - 1));
+            result.push(sample[num]);
+            sample.splice(num, 1);
+            permutationAtPos(position % factorial(sample.length), sample);
+        }
     }
     return result;
 }
 
-
-
 // Running with exercise specifics :
 result = permutationAtPos(position, sample);
 
-
-
 // Displaying result.
-console.log(result.join(''))
+console.log(result.join(''));
 
 // Display time the program ran.
 console.log("Duration : ", Date.now() - startTime, "ms");
